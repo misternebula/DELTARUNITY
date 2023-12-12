ebd5277823121b2d22c0f7efb43dcafb96df2cbc
_nebula <41904486+misternebula@users.noreply.github.com>
2023-12-12 02:19:15 +0000

    add asset index compat to PUSH

--- Assets/VirtualMachineRunner/VMExecuter.cs
+++ Assets/VirtualMachineRunner/VMExecuter.cs
@@ -421,21 +421,28 @@ namespace Assets.VirtualMachineRunner
 									variableName = variableName[10..]; // skip [stacktop]
 								}
 
-								var isGlobal = variableName.StartsWith("global.");
-								var isLocal = variableName.StartsWith("local.");
-								var isSelf = variableName.StartsWith("self.");
+								bool isGlobal = false, isLocal = false, isSelf = false, isAssetIndex = false;
+								var assetIndex = -1;
+								var split = variableName.Split('.');
+								var context = split[0];
+								variableName = split[1];
 
-								if (isGlobal)
+								if (context == "global")
 								{
-									variableName = variableName[7..];
+									isGlobal = true;
 								}
-								else if (isLocal)
+								else if (context == "local")
 								{
-									variableName = variableName[6..];
+									isLocal = true;
 								}
-								else if (isSelf)
+								else if (context == "self")
 								{
-									variableName = variableName[5..];
+									isSelf = true;
+								}
+								else if (int.TryParse(context, out var index))
+								{
+									isAssetIndex = true;
+									assetIndex = index;
 								}
 
 								if (isGlobal)
@@ -512,6 +519,21 @@ namespace Assets.VirtualMachineRunner
 									else
 									{
 										Ctx.Stack.Push(VariableResolver.GetSelfVariable(Ctx.Self, Ctx.Locals, variableName));
+									}
+								}
+								else if (isAssetIndex)
+								{
+									// TODO : GM probably gets the "first" instance by lowest instance id or something.
+									var firstInstance = InstanceManager.Instance.FindByAssetId(assetIndex).First();
+
+									if (indexingArray)
+									{
+										// TODO : work out how this works
+										return (ExecutionResult.Failed, $"Don't know how to push arrayed asset index variable {variableName}");
+									}
+									else
+									{
+										Ctx.Stack.Push(VariableResolver.GetSelfVariable(firstInstance, Ctx.Locals, variableName));
 									}
 								}
 								else
