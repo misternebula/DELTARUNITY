1be9f10cefd67a8a3f51538ea78f2f691436d3d7
_nebula <41904486+misternebula@users.noreply.github.com>
2023-12-18 17:13:00 +0000

    add obj_fadeout

---
+++ Assets/AssemblyScripts/obj_fadeout/gml_Object_obj_fadeout_Create_0.asm
@@ -0,0 +1,29 @@
+.localvar 0 arguments
+
+:[0]
+pushi.e 0
+pop.v.i self.image_blend
+push.d 0.08
+pop.v.d self.fadespeed
+pushi.e 0
+pop.v.i self.image_alpha
+pushi.e 10
+pushbltn.v self.room_width
+pushi.e 4
+conv.i.d
+div.d.v
+add.v.i
+pop.v.v self.length
+pushi.e 10
+pushbltn.v self.room_height
+pushi.e 4
+conv.i.d
+div.d.v
+add.v.i
+pop.v.v self.height
+pushi.e -20
+pop.v.i self.x
+pushi.e -20
+pop.v.i self.y
+
+:[end]
